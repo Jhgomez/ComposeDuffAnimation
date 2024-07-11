f802a98ed143c64440354b34da77d876983227bc
Juan Gomez <juan.e.g.f@outlook.com>
2024-07-10 23:59:55 -0600

    chore: add child animation

--- app/src/main/java/com/okiktech/porterduffanimation/MainActivity.kt
+++ app/src/main/java/com/okiktech/porterduffanimation/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -41,6 +42,11 @@ fun GhostTextAnimation() {
 
     val infiniteTransition = rememberInfiniteTransition()
 
+    val animatedOffset by infiniteTransition.animateFloat(
+        initialValue = ,
+        targetValue = ,
+        animationSpec = 
+    )
 }
 
 @Preview(showBackground = true)
