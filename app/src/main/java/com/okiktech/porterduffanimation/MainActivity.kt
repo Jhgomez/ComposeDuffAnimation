3adfc2d65e433d0a685f10428f50eee652e8f8f6
Juan Gomez <juan.e.g.f@outlook.com>
2024-07-10 23:13:22 -0600

    chore: context

--- app/src/main/java/com/okiktech/porterduffanimation/MainActivity.kt
+++ app/src/main/java/com/okiktech/porterduffanimation/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.okiktech.porterduffanimation.ui.theme.PorterDuffAnimationTheme
 
@@ -29,6 +30,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GhostTextAnimation() {
+    val context = LocalContext.current
 
 }
 
