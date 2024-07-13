b1e0f093f262e753afe44d871a1a789345736c4f
Juan Gomez <juan.e.g.f@outlook.com>
2024-07-12 19:23:18 -0600

    chore: add composable with graphic layer to do animations and create a text bit map

--- app/src/main/java/com/okiktech/porterduffanimation/MainActivity.kt
+++ app/src/main/java/com/okiktech/porterduffanimation/MainActivity.kt
@@ -14,13 +14,21 @@ import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.graphics.Canvas
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
@@ -104,6 +112,21 @@ fun GhostTextAnimation() {
             /* dstWidth = */ (textWidth / 2).toInt(),
             /* dstHeight = */ (textWidth / 2).toInt(),
             /* filter = */ true
+        )
+
+        Spacer(
+            modifier = Modifier
+                .size(size = 400.dp)
+                .graphicsLayer(scaleX = animatedScale, scaleY = animatedScale)
+                .drawWithContent {
+                    drawContent()
+
+                    val textBitmap = Bitmap.createBitmap(
+                        size.width.toInt(),
+                        size.height.toInt(),
+                        Bitmap.Config.ARGB_8888
+                    )
+                }
         )
     }
 }
